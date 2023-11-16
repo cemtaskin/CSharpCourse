0b0042a461918068a90716f6178aef4a4553124d
cemtaskin <cemtaskin82@gmail.com>
2023-11-16 22:34:39 +0300

    Loop 3 -for each

---
+++ Loop3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> names = new List<string>() { "Baran", "Batuhan", "Baturhan", "Harun" };
+
+            foreach(String name in names)
+            {
+                Console.WriteLine(name);
+                Console.WriteLine(name.Length);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
